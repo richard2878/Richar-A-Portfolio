ee403b4ba4f39cc4e112351466b475282339173b
Richard Antolin <richardantolin@gmail.com>
2020-10-14 10:11:48 -0700

    revised pages

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <Wrapper>
+            <Route exact path="/" component={About} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/portfolio" component={Portfolio} />
+
+          </Wrapper>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
