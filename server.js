ec07ab13418094bd2a9d1b740d9d1c4e50837664
Richard Antolin <richardantolin@gmail.com>
2020-10-16 10:38:01 -0700

    server set up

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8888;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes
+app.use(routes);
+
+// Start the server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> Server now listening on ${PORT}`);
+});
